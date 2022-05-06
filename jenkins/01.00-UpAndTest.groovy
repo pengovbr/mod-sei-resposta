fd2cd23150a007fa7e02725405735b804e634fb8
Marcelo Linhares Castro <marlinhares@gmail.com>
2022-05-06 14:45:50 -0300

    Jobs Jenkins

--- jenkins/01.00-UpAndTest.groovy
+++ jenkins/01.00-UpAndTest.groovy
@@ -1,27 +1,26 @@
 /*
 
 Pipeline Jenkins que automatiza checkout, up e roda os testes 1x apenas
-No seu cluster selenium vc precisa de um agente com a label SUPERGD e o user do agente precisa ter permissao de sudo, 
-alem do docker e docker-compose
-Nao recomendado rodar no jenkins master pois o Makefile precisa alterar a data da vm durante a execucao
+No seu cluster selenium vc precisa de um agente com a label SUPER-RESPOSTA, e tb instalado o
+o docker e docker-compose
 
 */
 
 pipeline {
     agent {
-      node {
-        label 'SUPERRESPOSTA'
-      }
+        node{
+            label "SUPER-RESPOSTA"
+        }
     }
-    
-    parameters { 
+
+    parameters {
         choice(
             name: 'database',
-            choices: "mysql\noracle\nsqlserver",                
+            choices: "mysql\noracle\nsqlserver",
             description: 'Qual o banco de dados' )
         string(
             name: 'urlGit',
-            defaultValue:"https://github.com/spbgovbr/mod-resposta.git",
+            defaultValue:"https://github.com/spbgovbr/mod-sei-resposta.git",
             description: "Url do git onde se encontra o módulo")
         string(
             name: 'credentialGit',
@@ -33,23 +32,23 @@ pipeline {
 	          description: "Branch/Versao do git onde se encontra módulo")
 	      string(
 	          name: 'sourceSuperLocation',
-	          defaultValue:"~/sei/FontesSEIcopia",
+	          defaultValue:"~/super/FonteSuper",
 	          description: "Localizacao do fonte do Super no servidor onde vai rodar o job")
-	      
+
     }
-    
+
     stages {
 
         stage('Inicializar Job'){
             steps {
-                
-                script{                    
+
+                script{
                     DATABASE = params.database
                     GITURL = params.urlGit
 					          GITCRED = params.credentialGit
 					          GITBRANCH = params.branchGit
                     SUPERLOCATION = params.sourceSuperLocation
-                    
+
                     if ( env.BUILD_NUMBER == '1' ){
                         currentBuild.result = 'ABORTED'
                         warning('Informe os valores de parametro iniciais. Caso eles n tenham aparecido faça login novamente')
@@ -60,63 +59,58 @@ pipeline {
                 sh """
                 echo ${WORKSPACE}
                 ls -lha
-                
+
                 make destroy || true
-                
+
                 """
             }
         }
-        
+
         stage('Checkout'){
-            
+
             steps {
-                
+
               sh """
-              
+
               git config --global http.sslVerify false
               """
-                
+
                 git branch: GITBRANCH,
                     credentialsId: GITCRED,
                     url: GITURL
-                
+
                 sh """
-                
+
                 ls -l
-				
+
                 """
             }
         }
-        
+
         stage('Build Env - Run Tests'){
-        
+
             steps {
-                
-                
+
+
                 sh """
                 ls
                 rm -rf .env
                 rm -rf .testselenium.env
-                
+
                 make base="${DATABASE}" config
                 make .testselenium.env
-                
-                sed -i "s|export SELENIUMTEST_RETRYTESTS=5|export SELENIUMTEST_RETRYTESTS=1|" .testselenium.env             
+
                 sed -i "s|SEI_PATH=../../../../|SEI_PATH=${SUPERLOCATION}|" .env
                 
-                if [ "${DATABASE}" == "oracle" ]; then
-                    
-                    sed -i "s|export SELENIUMTEST_RESTART_DB=false|export SELENIUMTEST_RESTART_DB=true|" .testselenium.env      
-                    
-                fi
-                
+                make up
+                make install
                 make MSGORIENTACAO=n tests-functional-full
-                
-                
-                
-                """  
-              
-            }        
+
+
+
+                """
+
+            }
         }
     }
 }
--- jenkins/02.00-UpAndTestsRetry.groovy
+++ jenkins/02.00-UpAndTestsRetry.groovy
@@ -1,29 +1,26 @@
 /*
 
 Pipeline Jenkins que automatiza o job 01.00
-Em caso de erro ele faz nova tentativa de execucao limitada a quantidade de tentativas informada 
+Em caso de erro ele faz nova tentativa de execucao limitada a quantidade de tentativas informada
 na execucao
 Util para rodar de forma assincrona pois os testes eventualmente possuem falso positivo
-No seu cluster selenium vc precisa de um agente com a label SUPERGD e o user do agente precisa ter permissao de sudo, 
+No seu cluster selenium vc precisa de um agente com a label SUPERGD e o user do agente precisa ter permissao de sudo,
 alem do docker e docker-compose
 
 */
 
 pipeline {
-    agent {
-        node {
-            label 'SUPERRESPOSTA'
-        }
-    }
-    
-    parameters { 
+    agent any
+
+
+    parameters {
         choice(
             name: 'database',
-            choices: "mysql\noracle\nsqlserver",                
+            choices: "mysql\noracle\nsqlserver",
             description: 'Qual o banco de dados' )
         string(
             name: 'urlGit',
-            defaultValue:"https://github.com/spbgovbr/mod-resposta.git",
+            defaultValue:"https://github.com/spbgovbr/mod-sei-resposta.git",
             description: "Url do git onde se encontra o módulo")
         string(
             name: 'credentialGit',
@@ -35,28 +32,28 @@ pipeline {
 	          description: "Branch/Versao do git onde se encontra módulo")
 	      string(
 	          name: 'sourceSuperLocation',
-	          defaultValue:"~/sei/FontesSEIcopia",
+	          defaultValue:"~/super/FonteSuper",
 	          description: "Localizacao do fonte do Super no servidor onde vai rodar o job")
 	      string(
 	          name: 'qtdTentativas',
 	          defaultValue:"5",
 	          description: "Quantidade de tentativas caso o teste falhe")
-          
+
     }
-    
+
     stages {
 
         stage('Inicializar Job'){
             steps {
-                
-                script{                    
+
+                script{
                     DATABASE = params.database
                     GITURL = params.urlGit
 					          GITCRED = params.credentialGit
 					          GITBRANCH = params.branchGit
                     SUPERLOCATION = params.sourceSuperLocation
                     QTDTENTATIVAS = params.qtdTentativas
-                    
+
                     if ( env.BUILD_NUMBER == '1' ){
                         currentBuild.result = 'ABORTED'
                         warning('Informe os valores de parametro iniciais. Caso eles n tenham aparecido faça login novamente')
@@ -66,19 +63,19 @@ pipeline {
 
                 sh """
                 echo ${WORKSPACE}
-                 
+
                 """
             }
         }
 
         stage('Build Env - Run Tests'){
-        
-            steps {    
+
+            steps {
 
                 retry(QTDTENTATIVAS){
-  
-                    build job: '01.00-UpAndTest.groovy', 
-                        parameters: 
+
+                    build job: '01.00-UpAndTest.groovy',
+                        parameters:
                             [
                                 string(name: 'database', value: DATABASE),
                                 string(name: 'urlGit', value: GITURL),
@@ -89,8 +86,8 @@ pipeline {
                 }
 
             }
- 
+
         }
-        
+
     }
 }

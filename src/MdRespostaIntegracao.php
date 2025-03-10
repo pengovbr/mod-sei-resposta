20c1ba9955ca9749030b1e4eee99a5b0c91e27d4
Carlos Vieira <3831408+caduvieira@users.noreply.github.com>
2025-03-10 14:14:15 -0300

    chore: cria target de compatibilidade
    Altera workflow de publicação
    
    Refs pengovbr/pen-apoio#17

--- src/MdRespostaIntegracao.php
+++ src/MdRespostaIntegracao.php
@@ -10,10 +10,6 @@ class MdRespostaIntegracao extends SeiIntegracao{
     '5.0.0'
   ];
 
-  const COMPATIBILIDADE_MODULO_SEI = [
-    '5.0.0'
-  ];
-
   public function getNome(){
     return 'Módulo de Resposta';
   }

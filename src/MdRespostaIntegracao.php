13d7fd37351a43d4fa45ab95a69aca043da5a462
Higo Cavalcante <higodf@gmail.com>
2026-05-12 13:23:50 -0300

    chore: Alteração no array de compatibilidade do módulo

--- src/MdRespostaIntegracao.php
+++ src/MdRespostaIntegracao.php
@@ -7,7 +7,7 @@ class MdRespostaIntegracao extends SeiIntegracao{
 
 
   const COMPATIBILIDADE_MODULO_SEI = [
-    '5.0.0', '5.0.1', '5.0.2', '5.0.3', '5.1.0'
+    '5.0.0', '5.0.1', '5.0.2', '5.0.3', '5.0.4', '5.1.0'
   ];
 
   public function getNome(){
